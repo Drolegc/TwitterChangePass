c624eeadd409d9757637d1a67e55246c889e3442
=drolegc <drolegc@gmail.com>
2019-07-31 02:37:05 -0300

    Second commit

--- TwitterRequest.py
+++ TwitterRequest.py
@@ -1,12 +1,3 @@
-#https://twitter.com/login?prefetchTimestamp=1564457669471
-
-#PASS EMAIL inxlkadxqhwkhovs
-
-#Primero - intentar logearnos
-
-#session[username_or_email]: drolegc@gmail.com
-#session[password]: 093364252
-#authenticity_token: 1548968d6fa7c8f15628e712450613b677889230
 
 import requests
 from bs4 import BeautifulSoup as bs
@@ -14,10 +5,9 @@ LOGIN_URL = 'https://twitter.com/login'
 RESTART_URL = 'https://twitter.com/account/begin_password_reset'
 CONFIRM_URL = 'https://twitter.com/account/send_password_reset'
 
-RESTABLECER_PASS_URL = 'https://twitter.com/i/redirect?url=https%3A%2F%2Ftwitter.com%2Faccount%2Fconfirm_email_reset%3Freset_type%3De%26user_id%3D1156326580291100674%26token%3Dbz4WgoWsWzJvtAUy1UbDJXf9oQBuvR_p8tJvzF8GQkY%253D-1564525099795%26confirm_pending_email%3D0%26token_version%3D0%26password_reset_cause%3Duser%26cn%3DcGFzc3dvcmRfcmVzZXRfdjI%253D&t=1+1564525099806&cn=cGFzc3dvcmRfcmVzZXRfdjI%3D&sig=58ce54dc4514d7441763f28f0cb4cbe0d122c089&iid=1e2ae650d5294b4bb807e6e26e1a93c0&uid=1156326580291100674&nid=248+1393'
+new_pass_url = ''
 RESET_PASS = 'https://twitter.com/account/reset_password'
 
-#Los datos que necesitaremos en el formulario
 
 def printR(r):
     soup = bs(r.content,'html5lib')
@@ -27,14 +17,14 @@ def printR(r):
 form_data = {
     'authenticity_token':'',
     'account_identifier':'tucartel.uruguay@gmail.com',
-    #authenticity_token lo obtenemos del html
+    
 }
 
 form_data_continue = {
     'authenticity_token': '',
     #'method_hint[1524856827]': 'device',
     'method_hint[132848851]': 'email',
-    'method': '132848851', #EL METODO ELEJIDO
+    'method': '132848851', 
 }
 
 form_data_end = {
@@ -53,19 +43,24 @@ print(':: Twitter Test :::')
 with requests.Session() as s:
     
     r = s.get(RESTART_URL)
+    
     printR(r)
     soup = bs(r.content,'html5lib')
     form_data['authenticity_token'] = soup.find('input',attrs={'name':'authenticity_token'})['value']
+    
     r = s.post(RESTART_URL,data=form_data,headers=headers)
     printR(r)
     
     form_data_continue['authenticity_token'] = form_data['authenticity_token']
+
     r = s.post(CONFIRM_URL,data=form_data_continue,headers=headers)
     printR(r)
     
     print('::: NEW PASS :::')
-    
-    r = s.get(RESTABLECER_PASS_URL)
+
+    new_pass_url = raw_input('Insert token link (The link from de button in the email) \n')
+
+    r = s.get(new_pass_url)
     printR(r)
 
     form_data_end['authenticity_token'] = form_data_continue['authenticity_token']
@@ -74,12 +69,5 @@ with requests.Session() as s:
     r = s.post(RESET_PASS,data=form_data_end,headers=headers)
     printR(r)
     
-
-#authenticity_token: 08943f88d9864107a1bbd4973dd82e3713e34cef
-#method_hint[1524856827]: device
-#method_hint[109976781]: email
-#method: 109976781 EL METODO ELEJIDO
-
-#Buscar el link en el email mandado
 
 
